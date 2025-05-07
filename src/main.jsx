e4a987157041c747ee94ea8be830cf95f7ceaafb
LocoIcee <62861210+LocoIcee@users.noreply.github.com>
2025-05-07 17:14:14 -0600

    Up

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import './components/website/utils/websiteStyles.css'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
