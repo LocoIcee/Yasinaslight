a3f618b22d669dd37fad3a4ed4f70474a1487cd8
LocoIcee <62861210+LocoIcee@users.noreply.github.com>
2025-05-08 00:09:19 -0600

    up

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Yasinaslight/',
+  base: '/Yasinaslight',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
