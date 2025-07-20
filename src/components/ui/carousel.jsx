b24bc468f0d9a9d8ee757b91c2e8f30a45e457ce
Cam <cameron13@live.ca>
2025-07-20 13:10:07 -0600

    carousel fix

--- src/components/ui/carousel.jsx
+++ src/components/ui/carousel.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 const ImageCarousel = ({ images, onClose }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
